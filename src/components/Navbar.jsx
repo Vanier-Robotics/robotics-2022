f7cf7216dc1f5b14e0cb6c975c70e25735a4fa44
shadowlerone <andrae.lewis@gmail.com>
2022-03-25 00:02:09 -0400

    rewrote navbar CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar(props) {
             <div className='navbar'>
                 <Link to='#' className='menu-bars'>
                     {/* <FaIcons.FaBars onClick={showSidebar} /> */}
-                    <img alt='menu bars' src={require('../assets/media/images/bars.png')} width='150px' onClick={props.showSidebar}/>
+                    <img alt='menu bars' src={require('../assets/media/images/bars.png')} onClick={props.showSidebar}/>
                 </Link>
             </div>
             <nav className={props.sidebar ? 'nav-menu active' : 'nav-menu'}>
@@ -43,62 +43,62 @@ function Navbar(props) {
                     </li>
                     <li className='nav-text'>
                         <Link to='#'>
-                            <span onClick={toggleLang}>{props.t('Language')}</span>
+                            <div onClick={toggleLang}>{props.t('Language')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/'>
                             <FaIcons.FaHome size={40} />
-                            <span>{props.t('Home')}</span>
+                            <div>{props.t('Home')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/menu'>
                             <GiIcons.GiTeapot size={50} />
-                            <span>{props.t('Menu')}</span>
+                            <div>{props.t('Menu')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/robot'>
                             <FaIcons.FaRobot size={50} />
-                            <span>{props.t('Robot')}</span>
+                            <div>{props.t('Robot')}</div>
                         </Link>
                     </li>
 
                     <li className="nav-text">
                         <Link to='/arcanum'>
                             <BsIcons.BsGearWideConnected size={50} />
-                            <span>{props.t('Arcanum')}</span>
+                            <div>{props.t('Arcanum')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/journey'>
                             <GiIcons.GiRabbitHead size={80} />
-                            <span>{props.t('Journey')}</span>
+                            <div>{props.t('Journey')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/characters'>
                             <HeroIcons.HiUsers size={46} />
-                            <span>{props.t('Characters')}</span>
+                            <div>{props.t('Characters')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/vaniercollege'>
                             <Io5Icons.IoSchoolSharp size={40} />
-                            <span>{props.t('VC')}</span>
+                            <div>{props.t('VC')}</div>
                         </Link>
                     </li>
                     <li className="nav-text">
                         <Link to='/jackpot'>
                             <GiIcons.GiHoneypot size={50} />
-                            <span>{props.t('Jackpot')}</span>
+                            <div>{props.t('Jackpot')}</div>
                         </Link>
                     </li>
                     <li className="nav-text" id='gallery' style={{display: `none`, borderStyle: `dashed`}}>
                         <Link to='/gallery'>
                             <GrIcons.GrGallery style={{filter: `invert(1)`}} size={40} />
-                            <span>{props.t('Gallery')}</span>
+                            <div>{props.t('Gallery')}</div>
                         </Link>
                     </li>
                 </ul>
